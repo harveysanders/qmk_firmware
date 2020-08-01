6f20c97ece4613d50a4f0c6be6b02e22838f2a1a
May Do <may.tdo15@gmail.com>
2020-07-31 19:24:06 -0500

    fix encoder index arg

--- keyboards/domo/keymaps/default/keymap.c
+++ keyboards/domo/keymaps/default/keymap.c
@@ -41,10 +41,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 
-void encoder_update_user(bool clockwise) {
-    if (clockwise) {
-        tap_code(KC_VOLU);
-    } else {
-        tap_code(KC_VOLD);
+void encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
     }
 }
