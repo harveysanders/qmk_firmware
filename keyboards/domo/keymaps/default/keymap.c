80f341b0e68f0d77442dda0662209707e053b59b
May Do <may.tdo15@gmail.com>
2020-08-16 12:17:04 -0500

    change default keymap to be one basic base layer

--- keyboards/domo/keymaps/default/keymap.c
+++ keyboards/domo/keymaps/default/keymap.c
@@ -1,43 +1,11 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Keymap 0: (Base Layer) Default Layer
-     * ,-------------------.
-     * |    |HOME|END |PGUP|
-     * |    |----|----|----|
-     * |    | TG | UP |PGDN|
-     * |----|----|----|----|
-     * | RO |LEFT|DOWN|RIGH|
-     * `-------------------'
-     *
-     *  |                   | Home          | End  | Page Up   |
-     *  |                   | Toggle: Layer | Up   | Page Down |
-     *  | Rotary: Vol Dn/Up | Left          | Down | Right     |
-     *    (Press: Mute)
-     */
+    // Keymap 0: (Base Layer) Default Layer
     [0] = LAYOUT(
         KC_HOME, KC_END, KC_PGUP,
-        TG(1), KC_UP, KC_PGDOWN,
+        KC_DEL, KC_UP, KC_PGDOWN,
         KC_MUTE, KC_LEFT, KC_DOWN, KC_RGHT
-    ),
-
-    /* Keymap 1: Function Layer
-     * ,-------------------.
-     * |    |BACK|FORW|REFR|
-     * |    |----|----|----|
-     * |    | TG |CUT |COPY|
-     * |----|----|----|----|
-     * |TRNS|UNDO|REDO|PSTE|
-     * `-------------------'
-     *
-     *  |             | Browser Back  | Browser Forward | Browser Refresh |
-     *  |             | Toggle: Layer | Cut             | Copy            |
-     *  | Transparent | Undo          | Redo            | Paste           |
-     */
-    [1] = LAYOUT(
-        KC_WBAK, KC_WFWD, KC_WREF,
-        TG(0), KC_CUT, KC_COPY,
-        KC_TRNS, KC_UNDO, KC_AGAIN, KC_PASTE
     ),
 };
 
